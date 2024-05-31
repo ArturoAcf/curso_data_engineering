b76b445fc7051d5b407b9a4a2d9d769655cd539b
Arturo Alonso <artunejo@hotmail.es>
2024-05-31 16:54:09 +0000

    Macro creada

--- macros/nothing_to_null.sql
+++ macros/nothing_to_null.sql
@@ -1,3 +1,5 @@
 {% macro nothing_to_null(column_name) %}
     IFF({{column_name}} = '', null, {{column_name}})
 {% endmacro %}
+
+-- https://discourse.getdbt.com/t/how-do-we-pass-column-values-into-dbt-macros/9782
